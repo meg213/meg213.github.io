0f30d27f630bd55d525e6d0df2c1102eccfccf4a
mgomez <mgomez46@gatech.edu>
2020-05-03 16:11:44 -0400

    banner

---
+++ mgomez-portfolio/src/components/banner.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "../styles/styles.scss"
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="container">
+                <div className="main-text">
+                    Hi, I'm Morgan Gomez, a Computational Media major at Georgia Tech.
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
--- mgomez-portfolio/src/pages/index.js
+++ mgomez-portfolio/src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "../styles/styles.scss"
 import Header from "../components/header"
+import Banner from "../components/banner"
 
 const IndexPage = () => (
  <div>
    <Header/>
+   <Banner/>
  </div>
 )
 
