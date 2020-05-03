a33b45ccd9c8a92188d6e73139a8ce65940ef9a9
mgomez <mgomez46@gatech.edu>
2020-05-03 16:40:11 -0400

    social media file added

---
+++ mgomez-portfolio/src/components/social.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "../styles/styles.scss"
+
+const Social = () => {
+    return (
+        <div className="social">
+            <div className="container">
+                <div className="main-text">
+                    Social media Content here
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Social;
--- mgomez-portfolio/src/pages/index.js
+++ mgomez-portfolio/src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import "../styles/styles.scss"
 import Header from "../components/header"
 import Banner from "../components/banner"
+import Social from "../components/social"
 
 const IndexPage = () => (
  <div>
    <Header/>
+   <Social/>
    <Banner/>
  </div>
 )
